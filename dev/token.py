7a883c4b2ee2ffa27b6e2385078e78a49951e4f7
5hojib <yesiamshojib@gmail.com>
2025-06-01 07:07:17 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- dev/token.py
+++ dev/token.py
@@ -1,16 +1,19 @@
-import pickle
 import os
-from google_auth_oauthlib.flow import InstalledAppFlow
+import pickle
+
 from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 credentials = None
 __G_DRIVE_TOKEN_FILE = "token.pickle"
-__OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive",
-'https://www.googleapis.com/auth/youtube.upload',
-    'https://www.googleapis.com/auth/youtube.readonly',
-    'https://www.googleapis.com/auth/youtube.force-ssl']
+__OAUTH_SCOPE = [
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/youtube.upload",
+    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+]
 if os.path.exists(__G_DRIVE_TOKEN_FILE):
-    with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
+    with open(__G_DRIVE_TOKEN_FILE, "rb") as f:
         credentials = pickle.load(f)
         if (
             (credentials is None or not credentials.valid)
@@ -21,8 +24,9 @@ if os.path.exists(__G_DRIVE_TOKEN_FILE):
             credentials.refresh(Request())
 else:
     flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', __OAUTH_SCOPE)
+        "credentials.json", __OAUTH_SCOPE
+    )
     credentials = flow.run_local_server(port=0, open_browser=False)
 
-with open(__G_DRIVE_TOKEN_FILE, 'wb') as token:
+with open(__G_DRIVE_TOKEN_FILE, "wb") as token:
     pickle.dump(credentials, token)
