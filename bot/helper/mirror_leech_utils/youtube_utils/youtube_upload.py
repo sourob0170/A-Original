00733e4d4667382d67cdf6f775e60f3fe60c2503
Mehedi Hasan Shojib <yesiamshojib@gmail.com>
2025-06-08 00:48:19 +0600

    Update youtube_upload.py

--- bot/helper/mirror_leech_utils/youtube_utils/youtube_upload.py
+++ bot/helper/mirror_leech_utils/youtube_utils/youtube_upload.py
@@ -314,7 +314,7 @@ class YouTubeUpload(YouTubeHelper):
                                         )
                                     if video_result: # Store dict
                                         individual_video_urls.append(video_result)
-                                            processed_in_folder += 1
+                                        processed_in_folder += 1
                                     except Exception as e:
                                         LOGGER.error(
                                             f"Failed to upload {item_name} to playlist {original_playlist_id}: {e}"
